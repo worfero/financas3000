e4f1dab70502c4e87bb8525c65a8961dbf62e019
Henrique Wolf <henriquewolf1999@gmail.com>
2025-10-31 09:09:39 -0300

    start working on a generic addItem function

--- public/js/addFields.js
+++ public/js/addFields.js
@@ -1,71 +1,3 @@
-function addIncome() {
-        const incomePlace = document.getElementById('new-incomes');
-        const incomeAdd = document.getElementById('add-income-btn');
-        const incomeLabel = document.getElementById('new-income-name');
-
-        incomeAdd.addEventListener("click", () => {
-                const labelText = incomeLabel.value.trim() || "Ganho"; // default if empty
-
-                const newIncome = { name: labelText, value: 0 };
-
-                const finLength = finance.incomes.push(newIncome);
-                const newIndex = finLength - 1
-                const income = finance.incomes[newIndex];
-
-                const container = document.createElement("div");
-                container.className = "input-group mb-3 input-group-sm mx-auto";
-
-                const span = document.createElement("span");
-                span.className = "input-group-text";
-                span.textContent = labelText + ":";
-                span.id = "income-" + newIndex + "-label";
-
-                const input = document.createElement("input");
-                input.type = "number";
-                input.className = 'form-control';
-                input.value = 0;
-                input.id = "income-" + newIndex;
-                input.name = "income-" + newIndex;
-                input.ariaLabel = labelText;
-
-                const deleteBtn = document.createElement("button");
-                deleteBtn.type = "button";
-                deleteBtn.id = "del-income-btn-" + newIndex;
-                deleteBtn.className = "btn btn-outline-danger fw-bold";
-                deleteBtn.textContent = "x";
-
-                addRemoveIncomeEvent(deleteBtn, newIndex);
-
-                container.appendChild(span);
-                container.appendChild(input);
-                container.appendChild(deleteBtn);
-
-                incomePlace.appendChild(container);
-
-                incomeLabel.value = "";
-
-                updateDB(finance);
-                input.addEventListener("input", async (event) => {
-                        // gets new value from the input field
-                        const newValue = parseInt(event.target.value);
-                        // updates total income
-                        finance.totalIncome = finance.totalIncome - income.value + newValue;
-                        // updates respective income field
-                        income.value = newValue;
-
-                        // update balance
-                        finance.balance = finance.totalIncome - finance.totalFixedBills;
-
-                        // updates total income field in the client
-                        document.getElementById("balance").value = finance.balance;
-                        // updates total income field in the client
-                        document.getElementById("income-total").value = finance.totalIncome;
-
-                        updateDB(finance);
-                });
-        });
-}
-
 function addFixedBill() {
         const fixedBillPlace = document.getElementById('new-fixed-bills');
         const fixedBillAdd = document.getElementById('add-fixed-bill-btn');
@@ -134,7 +66,110 @@ function addFixedBill() {
         });
 }
 
+function getListType(type) {
+        switch (type) {
+                case "income":
+                        itemList = finance.incomes;
+                        break;
+                case "fixed-bill":
+                        itemList = finance.fixedBills;
+                        break;
+                default:
+                        console.log("Error: list not found");
+                        itemList = [];
+                        break;
+        }
+        return itemList;
+}
+
+function updateTotal(type, item, newValue) {
+        switch (type) {
+                case "income":
+                        finance.totalIncome = finance.totalIncome - item.value + newValue
+                        total = finance.totalIncome;
+                        break;
+                case "fixed-bill":
+                        finance.totalFixedBills = finance.totalFixedBills - item.value + newValue
+                        total = finance.totalFixedBills;
+                        break;
+                default:
+                        console.log("Error: type not found");
+                        total = 0;
+                        break;
+        }
+        return total;
+}
+
+function addItem(type) {
+        const place = document.getElementById('new-' + type + 's');
+        const addBtn = document.getElementById('add-' + type + '-btn');
+        const label = document.getElementById('new-' + type + '-name');
+        addBtn.addEventListener("click", () => {
+                const labelText = label.value.trim() || "Item";
+
+                const newItem = { name: labelText, value: 0 };
+
+                const itemList = getListType(type);
+
+                const finLength = itemList.push(newItem);
+                const newIndex = finLength - 1
+                const item = itemList[newIndex];
+
+                const container = document.createElement("div");
+                container.className = "input-group mb-3 input-group-sm mx-auto";
+
+                const span = document.createElement("span");
+                span.className = "input-group-text";
+                span.textContent = labelText + ":";
+                span.id = type + "-" + newIndex + "-label";
+
+                const input = document.createElement("input");
+                input.type = "number";
+                input.className = 'form-control';
+                input.value = 0;
+                input.id = type + "-" + newIndex;
+                input.name = type + "-" + newIndex;
+                input.ariaLabel = labelText;
+
+                const deleteBtn = document.createElement("button");
+                deleteBtn.type = "button";
+                deleteBtn.id = "del-" + type + "-btn-" + newIndex;
+                deleteBtn.className = "btn btn-outline-danger fw-bold";
+                deleteBtn.textContent = "x";
+
+                addRemoveIncomeEvent(deleteBtn, newIndex);
+
+                container.appendChild(span);
+                container.appendChild(input);
+                container.appendChild(deleteBtn);
+
+                place.appendChild(container);
+
+                label.value = "";
+
+                updateDB(finance);
+                input.addEventListener("input", async (event) => {
+                        // gets new value from the input field
+                        const newValue = parseInt(event.target.value);
+                        // updates total income and returns its value
+                        const total = updateTotal(type, item, newValue);
+                        // updates respective income field
+                        item.value = newValue;
+
+                        // update balance
+                        finance.balance = finance.totalIncome - finance.totalFixedBills;
+
+                        // updates total income field in the client
+                        document.getElementById("balance").value = finance.balance;
+                        // updates total income field in the client
+                        document.getElementById(type + "-total").value = total;
+
+                        updateDB(finance);
+                });
+        });
+}
+
 (function () {
-        addIncome();
+        addItem("income");
         addFixedBill();
 })()
