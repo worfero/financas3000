c299ed5542387788e8ff2a5f8185ba771a7329d2
Henrique Wolf <henriquewolf1999@gmail.com>
2025-11-24 16:41:09 -0300

    protect finances route by user authentication

--- routes/finance.js
+++ routes/finance.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const finance = require('../controllers/finance');
 const catchAsync = require('../utils/catchAsync');
+const { requiresAuth } = require('express-openid-connect');
 
 const router = express.Router();
 
 router.route('/')
-    .get(catchAsync(finance.index))
+    .get(requiresAuth(), catchAsync(finance.index))
 
 module.exports = router;
